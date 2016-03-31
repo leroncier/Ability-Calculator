de0e78a9eb4d29bad753ab35a438369d70d43cef
le roncier <leroncier@gmail.com>
2016-03-31 14:55:03 +0200

    Basic Lay out
    lay out, number of points, one stepper

--- Ability Calculator/ViewController.swift
+++ Ability Calculator/ViewController.swift
@@ -8,13 +8,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var totalPoints: UILabel!
+    @IBOutlet weak var race: UITextField!
+    
+    @IBOutlet weak var numberOfPoints: UISegmentedControl!
+    @IBAction func numberOfPointsChoice(sender: UISegmentedControl) {
+        if(numberOfPoints.selectedSegmentIndex == 0)
+        {
+            totalPoints.text = "10";
+        }
+        else if(numberOfPoints.selectedSegmentIndex == 1)
+        {
+            totalPoints.text = "15";
+        }
+        else if(numberOfPoints.selectedSegmentIndex == 2)
+        {
+            totalPoints.text = "20";
+        }
+        else if(numberOfPoints.selectedSegmentIndex == 3)
+        {
+            totalPoints.text = "25";
+        }
     }
+    
+    @IBOutlet weak var basicForce: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBAction func stepperForce(sender: UIStepper) {
+        basicForce.text = "\(Int(stepper.value))"
+        totalPoints.text = "\(Int(totalPoints.text)! + 1)"
+        print(totalPoints.text)
+    }
+    
+    var pickerRaces: [String] = ["Human", "Dwarf", "Elf", "Gnome", "Half-elf", "Half-orc", "Halfing", "Other"]
+    var picker = UIPickerView()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Picker View        
+        
+        race.delegate = self
 
+        race.inputView = picker
+        
+        // Connect data:
+        picker.dataSource = self
+        picker.delegate = self
+        
+        // Input data from the Array
+        race.text = pickerRaces[0]
+        
+        // Display number of total points by default
+        totalPoints.text = "15"
+        
+    }
+    
+    // Column count: use one column.
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Row count: rows equals array length.
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return pickerRaces.count
+    }
+    
+    // Return a string from the array for this row.
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return pickerRaces[row]
+        
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        race.text = pickerRaces[row]
+        self.view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +96,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
